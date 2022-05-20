f62a3b0d1b32df8420668849b84eea69caca302a
Avvco <36072419+Avvco@users.noreply.github.com>
2022-05-20 11:55:59 +0800

    Update decoder_4to16.v

--- decoder_4to16.v
+++ decoder_4to16.v
@@ -5,7 +5,7 @@ module today0519(IN, LED, ALL) ;
 	output [15:0] LED ;
 	wire [3:0] temp ;
 	
-	//temp used: 1 and 0
+	//valid temp : 0 and 2
 	decode2_4 d1({IN[3], 1'b0}, temp, ALL) ;
 	
 	decode3_8 d2(IN[2:0], LED[7:0], temp[0]) ;
